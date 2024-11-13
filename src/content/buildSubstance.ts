4fa83e38f83b640b1424622a02a0c88b0dc1e29f
Paul Bauer <Paulalexisbauer04@gmail.com>
2024-11-13 12:59:02 -0500

    add features
    • disable save functionality, seems pointless and may save code since I won't have to worry about keeping user input

--- src/content/buildSubstance.ts
+++ src/content/buildSubstance.ts
@@ -3,17 +3,22 @@ import { wait } from '../util/wait.js';
 export const buildSubstanceButton = (): HTMLElement => {
     const container = document.createElement('div');
     container.className = 'flex justify-end';
-    
     const button = document.createElement('button');
     button.className = 'px-4 py-2 shadow-sm rounded uppercase bg-blue-200 dark:bg-blue-800 hover:bg-blue-300 dark:hover:bg-blue-700 text-gray-800 dark:text-gray-200 transition-colors duration-200';
-    button.textContent = 'Build Substance';
+    button.innerHTML = `
+        <span>Build Substance</span>
+        <span class="material-icons text-sm">build</span>
+    `;
     button.onclick = async () => {
         console.log('Building Substance');
         button.classList.add('animate-bounce');
         await wait(500);
         button.classList.remove('animate-bounce');
         console.log('Substance Built');
-        button.textContent = 'Discard Substance';
+        button.innerHTML = `
+            <span>Built</span>
+            <span class="material-icons text-sm">check</span>
+        `;
     };
     
     container.appendChild(button);
--- src/content/notebook.ts
+++ src/content/notebook.ts
@@ -5,9 +5,24 @@ export const createNotebookContent = (): HTMLElement => { // note: this gets loa
   const content = document.createElement("div");
   content.className = "flex flex-col px-3 gap-2 items-end";
   const textArea = document.createElement("textarea");
+  let data = DataManager.loadOutputData() as OutputData;
+  if (data) {
+    textArea.value = `Sample Temperature: ${data.basic.temperature.sample}
+Average Temperature: ${data.basic.temperature.average}
+Sample Pressure: ${data.basic.pressure.sample}
+Average Pressure: ${data.basic.pressure.average}
+Sample Volume: ${data.basic.volume.sample}
+Average Volume: ${data.basic.volume.average}
+Total Energy: ${data.energy.total.sample}
+Average Energy: ${data.energy.total.average}
+Kinetic Energy: ${data.energy.kinetic.sample}
+Average Kinetic Energy: ${data.energy.kinetic.average}
+Potential Energy: ${data.energy.potential.sample}
+Average Potential Energy: ${data.energy.potential.average}`;
+  }
   textArea.id = "notebook-contents";
   textArea.className = `
-    w-full min-h-32 max-h-48 p-2 rounded text-sm bg-gray-100 dark:bg-gray-800 
+    w-full min-h-32 max-h-48 p-2 rounded text-sm bg-white dark:bg-gray-800 
     border border-gray-200 dark:border-gray-600 text-gray-800 dark:text-gray-200`;
   let clearBtn = document.createElement("button");
   clearBtn.className = `px-2 py-1 text-xs shadow-sm rounded font-light
@@ -21,37 +36,38 @@ export const createNotebookContent = (): HTMLElement => { // note: this gets loa
         <span>Clear Notebook</span>
         <span class="material-icons text-sm">delete</span>
     `;
-  let saveBtn = document.createElement("button");
-  saveBtn.id = "save-notebook";
-  saveBtn.className = `px-2 py-1 text-xs shadow-sm rounded font-light
-                hover:bg-white dark:hover:bg-gray-800
-                bg-gray-100 dark:bg-gray-700
-                text-gray-800 dark:text-gray-200
-                border border-gray-200 dark:border-gray-600
-                transition-colors duration-200 items-center
-                flex gap-1`;
-  saveBtn.innerHTML = `
-        <span>Save</span>
-        <span class="material-icons text-sm">save</span>
-    `;
+  // let saveBtn = document.createElement("button");
+  // saveBtn.id = "save-notebook";
+  // saveBtn.className = `px-2 py-1 text-xs shadow-sm rounded font-light
+  //               hover:bg-white dark:hover:bg-gray-800
+  //               bg-gray-100 dark:bg-gray-700
+  //               text-gray-800 dark:text-gray-200
+  //               border border-gray-200 dark:border-gray-600
+  //               transition-colors duration-200 items-center
+  //               flex gap-1`;
+  // saveBtn.innerHTML = `
+  //       <span>Save</span>
+  //       <span class="material-icons text-sm">save</span>
+  //   `;
   clearBtn.addEventListener("click", () => {
     textArea.value = "";
+    DataManager.removeOutputData();
   });
   let buttoncontainer = document.createElement("div");
   buttoncontainer.className = "flex justify-end items-center gap-2";
-  buttoncontainer.appendChild(saveBtn);
+  // buttoncontainer.appendChild(saveBtn);
   buttoncontainer.appendChild(clearBtn);
   content.appendChild(buttoncontainer);
   content.appendChild(textArea);
 
   
-  let saveButton = content.querySelector("#save-notebook") as HTMLButtonElement;
-  saveButton.addEventListener("click", () => {
-    let data = DataManager.loadOutputData() as OutputData;
-    if(textArea.value.length > 0){
-      DataManager.saveOutputData(data);
-    } else DataManager.removeOutputData();
-  });
+  // let saveButton = content.querySelector("#save-notebook") as HTMLButtonElement;
+  // saveButton.addEventListener("click", () => {
+  //   let data = DataManager.loadOutputData() as OutputData;
+  //   if(textArea.value.length > 0){
+  //     DataManager.saveOutputData(data);
+  //   } else DataManager.removeOutputData();
+  // });
 
   window.addEventListener('copy-notebook', () => {
     let data = DataManager.loadOutputData() as OutputData;
@@ -66,7 +82,7 @@ Total Energy: ${data.energy.total.sample}
 Average Energy: ${data.energy.total.average}
 Kinetic Energy: ${data.energy.kinetic.sample}
 Average Kinetic Energy: ${data.energy.kinetic.average}
-Potential Energy: ${data.energy.potential.sample}    
+Potential Energy: ${data.energy.potential.sample}
 Average Potential Energy: ${data.energy.potential.average}`;
     }
   });
